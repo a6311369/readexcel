2e792cc13e554609f5b7fb5366576f3d82e58c0e
a6311369 <a6311369@gmail.com>
2018-08-01 13:56:38 +0800

    Initial commit

---
+++ readexcel.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+import openpyxl
+
+# 讀取Excel
+workbook = openpyxl.load_workbook("C:\\Users\\jiunlin\Desktop\\test.xlsx")
+
+# 取得所有工作表
+worksheets = workbook.sheetnames
+
+# 取得第一個工作表
+sheet = workbook[worksheets[0]]
+i = 1
+
+# 所有的row loop
+for row in sheet.rows:
+    #標題跳過
+    if i == 1:
+        i = i + 1
+        continue
+
+    #判斷每筆row 的 第一個 cell是否為空 是的話跳過
+    if row[0].value is None:
+        i = i + 1
+        continue
+
+    #要比對的值
+    array = ["17","19","20","22","24","27","42","55"]
+
+    #將比對的值 loop 去跟 excel的值比對
+    for item in array:
+        #如果有比對的到會大於-1
+        if row[0].value.find(item) > -1:
+            #將值給印出來
+            print(row[0].value)
